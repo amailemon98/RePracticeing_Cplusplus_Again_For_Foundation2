375d51b507ef628e39428612d40949b23e76f7bd
Junhyung <amailemon98@naver.com>
2022-08-05 17:21:11 +0900

    새와 동물의 구조체 차이

---
+++ RePracticeing_C++_Again_For_Foundation2/prac.cpp
@@ -0,0 +1,21 @@
+typedef struct Bird {
+	char name[30];
+	int age;
+
+	int health;
+	int food;
+	int clean;
+
+	int height;
+}Bird;
+
+typedef struct Fish {
+	char name[30];
+	int age;
+
+	int health;
+	int food;
+	int clean;
+
+	int deep;
+}Fish;
