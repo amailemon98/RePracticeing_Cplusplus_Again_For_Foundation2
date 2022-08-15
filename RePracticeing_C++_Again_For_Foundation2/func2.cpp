aede8384f0e05db2fafd18348251d416d2c37d88
Junhyung <amailemon98@naver.com>
2022-08-15 16:27:48 +0900

    더하기 빼기 계산기 코드 완성

--- RePracticeing_C++_Again_For_Foundation2/func2.cpp
+++ RePracticeing_C++_Again_For_Foundation2/func2.cpp
@@ -2,20 +2,34 @@
 using std::cout; 
 using std::cin;
 
-int func1(int &_a, int &_b) {
-	int a = _a;
-	int b = _b;
-
+int Add(int &a, int &b) {
+	
+	cout << "값을 입력하세요" << '\n';
 	cin >> a;
 	cin >> b;
 
-	int c;
-	c = a + b;
-	cout << c << '\n';
+	a += b;
 
-	return 0;
+	return a;
 }
+
+int Sub(int &a, int &b) {
+	cout << "값을 입력하세요" << '\n';
+	cin >> a;
+	cin >> b;
+
+	a -= b;
+
+	return a;
+}
+
 int main() {
-	int a, b;
-	func1(a, b);
+	int a, b, start;
+	cout << "더하기는 1, 빼기는 2를 입력해주세요" << '\n';
+	cin >> start, '\n';
+	if (start == 1) {
+		cout << Add(a, b);
+	}else if (start == 2) {
+		cout << Sub(a, b);
+	}
 }
