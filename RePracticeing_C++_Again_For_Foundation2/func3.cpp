592320848fbcacb7f008e5c4d242479b36bbd9ec
Junhyung <amailemon98@naver.com>
2022-08-20 21:12:03 +0900

    func3.cpp 파일을 더 효율적으로 수정하였음.

--- RePracticeing_C++_Again_For_Foundation2/func3.cpp
+++ RePracticeing_C++_Again_For_Foundation2/func3.cpp
@@ -1,14 +1,18 @@
-/*#include <iostream>
+#include <iostream>
 
 void funcPrint() {
-	std::cout << "함수를 호출하였습니다." << '\n';
+	std::cout << "전달할 값을 입력해주세요." << '\n';
 }
 
-void paraPrint(int x) {
+void paraPrint(int &x) {
+	std::cout << "단일 매개변수 함수에 전달할 값 : " << '\n';
+	std::cin >> x;
 	std::cout << "매개변수에 전달 받은 값 : " << x << '\n';
 }
 
-void paraPrint(int x, double y) {
+void paraPrint(int &x, double &y) {
+	std::cout << "이중 매개변수 함수에 전달할 값 : " << '\n';
+	std::cin >> x >> y;
 	std::cout << "매개변수 X의 값은 : " << x << '\n';
 	std::cout << "매개변수 y의 값은 : " << y << '\n';
 }
@@ -17,9 +21,6 @@ int main() {
 	int para1, para2;
 	double para3;
 	funcPrint();
-	std::cin >> para1;
-	std::cin >> para2 >> para3;
 	paraPrint(para1);
 	paraPrint(para2, para3);
 }
-*/
