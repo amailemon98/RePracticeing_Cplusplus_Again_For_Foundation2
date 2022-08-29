9d385c8176b6faf29708a9f833ddff55ef48c83a
Junhyung <amailemon98@naver.com>
2022-08-29 11:25:58 +0900

    sstream 사용법 코드

---
+++ RePracticeing_C++_Again_For_Foundation2/sstream.cpp
@@ -0,0 +1,25 @@
+/*#include <iostream>
+#include <string>
+#include <sstream>
+
+int main() {
+	/*
+	std::string lines = "hello my name is";
+	std::stringstream ss(lines);
+	std::string tmp;
+
+	while ((ss >> tmp)) {
+		log.push_back(tmp);
+		std::cout << tmp << "\n";
+	}*/
+	/*
+	std::string lines = "2002:04:21";
+	std::istringstream iss(lines);
+	std::string token;
+	while (getline(iss, token, ":")) {
+		std::cout << token << "\n";
+	}
+}*/
+
+
+
